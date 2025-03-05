980f229171cbbb9c33de1ccfafb24602d2259f0f
356dogs <didyblckm@gmail.com>
2025-03-05 08:38:59 +0100

    separation de Carte dans un fichier dédié pour plus de modularité

--- Gestion_Grille.js
+++ Gestion_Grille.js
@@ -1,41 +1,4 @@
-class Carte{
-  constructor(ID, front_side,back_side){
-      this.ID = ID;
-      this.front_side = front_side;
-      this.back_side = back_side;
-  }    
-}
-
-// on a besoin de deux instances de la même carte pour former une paire
-let carte1_1 = new Carte(10, "placeholder.png", "back.png");
-let carte1_2 = new Carte(11, "placeholder.png", "back.png");
-
-let carte2_1 = new Carte(20, "placeholderazaza.png", "back.png");
-let carte2_2 = new Carte(21, "placeholderazaza.png", "back.png");
-
-let carte3_1 = new Carte(30, "placeholder.png", "back.png");
-let carte3_2 = new Carte(31, "placeholder.png", "back.png");
-
-let carte4_1 = new Carte(40, "placeholder.png", "back.png");
-let carte4_2 = new Carte(41, "placeholder.png", "back.png");
-
-let carte5_1 = new Carte(50, "placeholder.png", "back.png");
-let carte5_2 = new Carte(51, "placeholder.png", "back.png");
-
-let carte6_1 = new Carte(60, "placeholder.png", "back.png");
-let carte6_2 = new Carte(61, "placeholder.png", "back.png");
-
-let carte7_1 = new Carte(70, "placeholder.png", "back.png");
-let carte7_2 = new Carte(71, "placeholder.png", "back.png");
-
-let carte8_1 = new Carte(80, "placeholder.png", "back.png");
-let carte8_2 = new Carte(81, "placeholder.png", "back.png");
-
-let carte9_1 = new Carte(90, "placeholder.png", "back.png");
-let carte9_2 = new Carte(91, "placeholder.png", "back.png");
-
-let carte10_1 = new Carte(100, "placeholder.png", "back.png");
-let carte10_2 = new Carte(101, "placeholder.png", "back.png");
+import { Carte, cards } from './Gestion_carte.js';
 
 //js n'a pas de fonction randint(min,max) prédefinie donc on doit la creer
 function randomInt(min, max) {
@@ -97,8 +60,6 @@ function creation_grille(x,y){
   }  
   return grille
 }
-
-creation_grille(4,1);
 
 
 
